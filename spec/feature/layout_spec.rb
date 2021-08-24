d5e0f5b28ab63cebe814ea7cb644f58d56da8cb0
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-08-24 21:12:58 +0100

    Ensuring header is displayed on each page

--- spec/feature/layout_spec.rb
+++ spec/feature/layout_spec.rb
@@ -1,6 +1,12 @@
 feature 'standard layout' do
-  scenario 'shows the header on each page' do
+  scenario 'shows the header on the homepage' do
     visit '/'
+    expect(page).to have_content 'Makers BnB'
+
+    visit '/login'
+    expect(page).to have_content 'Makers BnB'
+    
+    visit '/signup'
     expect(page).to have_content 'Makers BnB'
   end
 end
