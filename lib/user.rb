2a4ec0205f16e447601212a5284a856a3576b0ff
Suhani Zaman <suhanizaman15@gmail.com>
2021-08-25 16:43:57 +0400

    create user creates new instance of user class

--- lib/user.rb
+++ lib/user.rb
@@ -2,14 +2,27 @@ require './database_connection_setup'
 require './lib/db_connection'
 
 class User
+  attr_reader :name, :email, :password, :id
+  def initialize(id,name,email,password)
+    @id = id
+    @name = name
+    @email = email
+    @password = password
+  end
+  def self.instance
+    @user
+  end
   def self.create(name, email, password)
     new_user = DBConnection.query(
       "INSERT INTO users(name, email, password)
         VALUES('#{name}', '#{email}', '#{password}')
         RETURNING *;"
       )
-
-    [:id, :name, :email, :password].zip(new_user.values.flatten).to_h
+    #p new_user.map { |user| [user['user_id'], user["name"], user['email'], user['password']]}
+     
+    data = ["user_id", "name", "email", "password"].map{|item| new_user[0][item]}
+    @user = User.new(*data)
+    #[:id, :name, :email, :password].zip(new_user.values.flatten).to_h
   end
 
   def self.authenticate(email, password)
--- spec/unit/user_id_spec.rb
+++ spec/unit/user_id_spec.rb
@@ -6,10 +6,10 @@ describe User do
 
     it 'creates a new user object with the required parameters' do
       user = User.create('User', 'Email', 'Passw0rd')
-      expect(user.name).to eq 'User'
-      expect(user.email).to eq 'Email'
-      expect(user.password).to eq 'Passw0rd'
-      expect(user.id).to eq '1'
+      expect(user.name).to eq "User"
+      expect(user.email).to eq "Email"
+      expect(user.password).to eq "Passw0rd"
+      expect(user.id.to_i).to be_a Integer
     end
 
   end
