4f50884c5d562a9dd18ccd57d23e118c3cdfb2ba
edpackard <edpackard81@gmail.com>
2021-08-27 14:09:17 +0100

    updated and fixed feature tests

--- spec/feature/booking_spec.rb
+++ spec/feature/booking_spec.rb
@@ -1,17 +1,19 @@
 feature 'booking' do
   scenario 'shows signed up users the option to make a booking' do
     create_new_user('User', 'user@example.com', 'P455w0rd')
-    Space.create('Home 1', 'Beautiful three bedroom house', '500', User.current.id)
-    visit '/spaces'
+    list_a_space
+    expect(page).to have_content 'Apartment'    
     expect(page).to have_content 'Book this space'
+    click_on 'logout'
   end
 
   scenario 'allows a user to make a booking' do
-    User.create('User', 'user@example.com', 'P455w0rd')
-    Space.create('Home 1', 'Beautiful three bedroom house', '500', User.current.id)
-    User.create('User2', 'user2@example.com', 'P455w0rd')
-
-    visit "/booking/new/#{Space.current.id}"
-    expect(page).to have_content "Book #{Space.current.name}"
+    create_new_user('User', 'user@example.com', 'P455w0rd')
+    list_a_space
+    click_on 'logout'
+    create_new_user('User2', 'user2@example.com', 'P455w0rd')
+    expect(page).to have_content "Apartment"
+    expect(page).to have_content "Book this space"
+    click_on 'logout'
   end
 end
